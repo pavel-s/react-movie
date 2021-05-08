d64190f850173b75d890e85d61250f0ce2c023a0
pavel-s <sergeevps@gmail.com>
2021-05-08 12:07:26 +0500

    add redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Movies from './routes/Movies/Movies';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store, { persistor } from './redux/store';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core';
 import './utils/theme';
 import WatchList from './routes/WatchList/WatchList';
 import Watched from './routes/Watched/Watched';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const theme = createMuiTheme({
   palette: { type: 'dark' },
@@ -17,21 +18,23 @@ const theme = createMuiTheme({
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <Route exact path='/'>
-              <Movies />
-            </Route>
-            <Route path='/watchlist'>
-              <WatchList />
-            </Route>
-            <Route path='/watched'>
-              <Watched />
-            </Route>
-          </Switch>
-        </Router>
-      </ThemeProvider>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Switch>
+              <Route exact path='/'>
+                <Movies />
+              </Route>
+              <Route path='/watchlist'>
+                <WatchList />
+              </Route>
+              <Route path='/watched'>
+                <Watched />
+              </Route>
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,22 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
 import moviesReducer from './moviesReducer';
 import userReducer from './userReducer';
 import watchedReducer from './watchedReducer';
 import watchListReducer from './watchListReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist/es/constants';
 
 const rootReducer = combineReducers({
   movies: moviesReducer,
@@ -11,12 +25,27 @@ const rootReducer = combineReducers({
   watched: watchedReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    // https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const persistor = persistStore(store);
 export default store;
